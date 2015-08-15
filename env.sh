1575a41d0af0ae7d31e8418bc8a4ac29f1ee650a
el <elarasu@gmail.com>
2015-08-14 17:20:59 -0700

    goroot not required

--- env.sh
+++ env.sh
@@ -4,6 +4,6 @@ alias killemacs="emacsclient -e '(kill-emacs)'"
 alias e="emacsclient -t"
 alias startemacs="emacs --daemon"
 # go lang related setup
-export GOROOT=/usr/local/go
+#export GOROOT=/usr/local/go
 export GOPATH=$HOME/test/golang
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
